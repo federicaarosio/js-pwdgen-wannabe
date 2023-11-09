a8a9ead7758778d465f711a97e61cf0c365ec64f
federicaarosio <arosio.fede@gmail.com>
2023-11-09 15:03:55 +0100

    Password ex.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const firstName = prompt("Qual è il tuo nome?");
+const lastName = prompt("Qual è il tuo cognome?");
+const favouriteColor = prompt("Qual è il tuo colore preferito?");
+
+document.getElementById("output").innerHTML = "Password: " + firstName + lastName + favouriteColor + "21";
